090626b7f420cd6a0c5ad7830b063e8a1223ec9c
priyanshi <priyanshisaxena628@gmail.com>
2022-03-30 18:27:39 +0530

    signUpController

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,24 +1,37 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.models.User;
 import com.example.demo.repositories.UserRepository;
 
 
-@Controller
+@RestController
 public class UserController {
 	
+	@Autowired
 	private UserRepository userRepo;
 	
-	@RequestMapping(method= RequestMethod.POST, value="/signup")
-	public ResponseEntity<?> studentSignUp(String email) {
-		if(userRepo.existsByEmail(email)) {
-			return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
-		}
-		//return null;
+
+	@PostMapping("/signup")
+	public User studentSignUp(@RequestBody User userData) {
+		//User res= null;
+//		if(userRepo.existsByEmail(email)) {
+//			//return new ResponseEntity<>("Email is already taken!", HttpStatus.BAD_REQUEST);
+//			res= "Email is already taken!";
+//			return res;
+//		}
+		System.out.println("Heyy!");
+		userRepo.save(userData);
+		return userData;
+		//return res;
 	}
 }
